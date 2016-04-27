6d7794ab87f75c0fd249edec46ac0de6de1ab1b5
Yury Vovk <y.vovk@drweb.com>
2016-04-27 13:53:05 +0300

    Apple style exceptions

--- OFUniversalException.h
+++ OFUniversalException.h
@@ -3,17 +3,44 @@
 
 @class OFString;
 @class OFConstantString;
+@class OFDictionary;
+
+#ifdef __cplusplus
+#define EXCEPTION_CONSTANT_EXPORT extern "C"
+#else
+#define EXCEPTION_CONSTANT_EXPORT extern
+#endif
+
+EXCEPTION_CONSTANT_EXPORT OFString *const kSourceFile;
+EXCEPTION_CONSTANT_EXPORT OFString *const kSourceFunction;
+EXCEPTION_CONSTANT_EXPORT OFString *const kSourceLine;
+EXCEPTION_CONSTANT_EXPORT OFString *const kSourceClass;
 
 @interface OFUniversalException: OFException
 {
 	OFString* _name;
-	OFString* _message;
+	OFString* _reason;
+	OFDictionary* _userInfo;
 }
 
-- (instancetype)initWithName:(OFString *)name message:(OFString *)message;
+@property(readonly, copy)OFString* name;
+@property(readonly, copy)OFString* reason;
+@property(readonly, copy)OFDictionary* userInfo;
+
 - (instancetype)initWithName:(OFString *)name format:(OFConstantString *)frmt, ...;
 - (instancetype)initWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args;
-+ (instancetype)exceptionWithName:(OFString *)name message:(OFString *)message;
+- (instancetype)initWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args userInfo:(OFDictionary *)userInfo;
 + (instancetype)exceptionWithName:(OFString *)name format:(OFConstantString *)frmt, ...;
++ (instancetype)exceptionWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args;
++ (instancetype)exceptionWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args userInfo:(OFDictionary *)userInfo;
 
+@end
+
+
+@interface OFException(UniversalException)
+- (void)raise;
++ (void)raise:(OFString *)name format:(OFConstantString *)frmt, ...;
++ (void)raise:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args;
++ (id)exceptionWithName:(OFString *)name reason:(OFString *)reason userInfo:(OFDictionary *)userInfo;
++ (void)showSourceInfo:(bool)flag;
 @end
--- OFUniversalException.m
+++ OFUniversalException.m
@@ -1,65 +1,163 @@
 #import <ObjFW/ObjFW.h>
 #import "OFUniversalException.h"
 
+OFString *const kSourceFile = @"SourceFile";
+OFString *const kSourceFunction = @"SourceFunction";
+OFString *const kSourceLine = @"SourceLine";
+OFString *const kSourceClass = @"SourceClass";
+
+@interface OFUniversalException()
+
+@property(readwrite, copy)OFString* name;
+@property(readwrite, copy)OFString* reason;
+@property(readwrite, copy)OFDictionary* userInfo;
+
+@end
+
+static bool __show_source_exception_info = false;
+
 @implementation OFUniversalException
 
-- (instancetype)initWithName:(OFString *)name message:(OFString *)message
-{
-	self = [super init];
-
-	_name = [name copy];
-	_message = [message copy];
-
-	return self;
-}
+@synthesize name = _name;
+@synthesize reason = _reason;
+@synthesize userInfo = _userInfo;
 
 - (instancetype)initWithName:(OFString *)name format:(OFConstantString *)frmt, ...
 {
-	self = [super init];
-
-	_name = [name copy];
-
+	OFUniversalException* exception;
 	va_list ap;
 	va_start(ap, frmt);
-	_message = [[OFString alloc] initWithFormat:frmt arguments:ap];
+	exception = [self initWithName:name format:frmt arguments:ap];
 	va_end(ap);
 
-	return self;
+	return exception;
 }
 
 - (instancetype)initWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args
 {
+	return [self initWithName:name format:frmt arguments:args userInfo:nil];
+}
+
+- (instancetype)initWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args userInfo:(OFDictionary *)userInfo;
+{
 	self = [super init];
 
-	_name = name;
+	void* pool = objc_autoreleasePoolPush();
 
-	_message = [[OFString alloc] initWithFormat:frmt arguments:args];
+	self.name = name;
+
+	self.reason = [OFString stringWithFormat:frmt arguments:args];
+
+	self.userInfo = userInfo;
+
+	objc_autoreleasePoolPop(pool);
 
 	return self;
 
 }
 
-+ (instancetype)exceptionWithName:(OFString *)name message:(OFString *)message
-{
-	return [[[self alloc] initWithName:name message:message] autorelease];
-}
 
 + (instancetype)exceptionWithName:(OFString *)name format:(OFConstantString *)frmt, ...
 {
+	OFUniversalException* exception;
 	va_list ap;
 	va_start(ap, frmt);
-	OFUniversalException* result = [[self alloc] initWithName:name format:frmt arguments:ap];
+	exception = [[self alloc] initWithName:name format:frmt arguments:ap];
 	va_end(ap);
 
-	return [result autorelease];
+	return [exception autorelease];
+}
+
++ (instancetype)exceptionWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args
+{
+	return [[[self alloc] initWithFormat:frmt arguments:args] autorelease];
+}
+
++ (instancetype)exceptionWithName:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args userInfo:(OFDictionary *)userInfo
+{
+	return [[[self alloc] initWithName:name format:frmt arguments:args userInfo:userInfo] autorelease];
 }
 
 - (OFString*)description
 {
-	if ([_name containsString:@"exception"] || [_name containsString:@"Exception"])
-		return [OFString stringWithFormat:@"%@ occurred! %@", _name, _message];
-	else
-		return [OFString stringWithFormat:@"%@ exception occurred! %@", _name, _message];
+	if (!__show_source_exception_info)
+		return [OFString stringWithFormat:@"%@", self.reason];
+	else {
+		OFMutableString* descriptionString = [OFMutableString string];
+
+		void* pool = objc_autoreleasePoolPush();
+
+		if (self.userInfo != nil) {
+			if (self.userInfo[kSourceClass])
+				[descriptionString appendFormat:@"<Class %@> ", self.userInfo[kSourceClass]];
+
+			if (self.userInfo[kSourceFile])
+				[descriptionString appendFormat:@"%@", self.userInfo[kSourceFile]];
+
+			if (self.userInfo[kSourceFunction])
+				[descriptionString appendFormat:@"::%@", self.userInfo[kSourceFunction]]; 
+
+			if (self.userInfo[kSourceLine])
+				[descriptionString appendFormat:@"[%@]", self.userInfo[kSourceFunction]];
+		}
+
+		if ([descriptionString length] > 0)
+			[descriptionString appendFormat:@" "];
+
+		[descriptionString appendFormat:@"%@", self.reason];
+
+		objc_autoreleasePoolPop(pool);
+
+		[descriptionString makeImmutable];
+
+		return descriptionString;
+
+	}
+}
+
+- (void)dealloc
+{
+	[_name release];
+	[_reason release];
+	[_userInfo release];
+
+	[super dealloc];
+}
+
+@end
+
+@implementation OFException(UniversalException)
+
+- (void)raise
+{
+	@throw self;
+}
+
++ (void)raise:(OFString *)name format:(OFConstantString *)frmt, ...
+{
+	OFUniversalException* exception;
+
+	va_list ap;
+	va_start(ap, frmt);
+	exception = [OFUniversalException exceptionWithName:name format:frmt arguments:ap];
+	va_end(ap);
+
+	@throw exception;
+}
+
++ (void)raise:(OFString *)name format:(OFConstantString *)frmt arguments:(va_list)args
+{
+	@throw [OFUniversalException exceptionWithName:name format:frmt arguments:args];
+}
+
++ (id)exceptionWithName:(OFString *)name reason:(OFString *)reason userInfo:(OFDictionary *)userInfo
+{
+	return [OFUniversalException exceptionWithName:name reason:reason userInfo:userInfo];
+}
+
++ (void)showSourceInfo:(bool)flag
+{
+	__show_source_exception_info = flag;
 }
 
 @end
