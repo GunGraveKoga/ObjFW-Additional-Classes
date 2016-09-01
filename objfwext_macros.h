c29c9e140d8a78f6709d7a0be7a6016eb8a40311
Yury Vovk <y.vovk@drweb.com>
2016-09-01 12:03:29 +0300

    + OF_ENUM && OF_OPTIONS macroses

--- objfwext_macros.h
+++ objfwext_macros.h
@@ -3,3 +3,16 @@
 #else
 #define OBJFW_EXTENSION_EXPORT extern
 #endif
+
+// Enums and Options
+#if (__cplusplus && __cplusplus >= 201103L && (__has_extension(cxx_strong_enums) || __has_feature(objc_fixed_enum))) || (!__cplusplus && __has_feature(objc_fixed_enum))
+  #define OF_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
+  #if (__cplusplus)
+    #define OF_OPTIONS(_type, _name) _type _name; enum : _type
+  #else
+    #define OF_OPTIONS(_type, _name) enum _name : _type _name; enum _name : _type
+  #endif
+#else
+  #define OF_ENUM(_type, _name) _type _name; enum
+  #define OF_OPTIONS(_type, _name) _type _name; enum
+#endif
